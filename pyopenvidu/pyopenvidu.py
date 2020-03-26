c89a9d4bc0cc67cb43e6baf01d03a6eec7123e95
marcsello <punkosdmarcell@rocketmail.com>
2020-03-26 01:30:36 +0100

    Implemented the first method

--- pyopenvidu/pyopenvidu.py
+++ pyopenvidu/pyopenvidu.py
@@ -1,5 +1,22 @@
 """Main module."""
+from requests_toolbelt.sessions import BaseUrlSession
+from requests.auth import HTTPBasicAuth
+from requests_toolbelt import user_agent
+from . import __version__
 
 
 class OpenVidu(object):
-    pass
+
+    def __init__(self, url: str, secret: str):
+        self._session = BaseUrlSession(base_url=url)
+        self._session.auth = HTTPBasicAuth('OPENVIDUAPP', secret)
+
+        self._session.headers.update({
+            'User-Agent': user_agent('PyOpenVidu', __version__)
+        })
+
+    def get_config(self):
+        r = self._session.get('config')
+        r.raise_for_status()
+
+        return r.json()
--- tests/test_pyopenvidu.py
+++ tests/test_pyopenvidu.py
@@ -3,21 +3,32 @@
 """Tests for `pyopenvidu` package."""
 
 import pytest
-
 from pyopenvidu import OpenVidu
+from urllib.parse import urljoin
+
+URL_BASE = 'http://test.openvidu.io:4443/'
+SECRET = 'MY_SECRET'
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def openvidu_instance():
+    yield OpenVidu(URL_BASE, SECRET)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_config(openvidu_instance, requests_mock):
+    original = {"version": "2.9.0", "openviduPublicurl": URL_BASE, "openviduCdr": False,
+                "maxRecvBandwidth": 1000, "minRecvBandwidth": 300, "maxSendBandwidth": 1000, "minSendBandwidth": 300,
+                "openviduRecording": True, "openviduRecordingVersion": "2.8.0",
+                "openviduRecordingPath": "/opt/openvidu/recordings/", "openviduRecordingPublicAccess": True,
+                "openviduRecordingNotification": "publisher_moderator",
+                "openviduRecordingCustomLayout": "/opt/openvidu/custom-layout/",
+                "openviduRecordingAutostopTimeout": 120, "openviduWebhook": True,
+                "openviduWebhookEndpoint": "http://localhost:7777/webhook/",
+                "openviduWebhookHeaders": ["Authorization: Basic YWJjZDphYmNk"],
+                "openviduWebhookEvents": ["recordingStatusChanged"]}
+
+    requests_mock.get(urljoin(URL_BASE, 'config'), json=original)
+
+    a = openvidu_instance.get_config()
+
+    assert a == original
