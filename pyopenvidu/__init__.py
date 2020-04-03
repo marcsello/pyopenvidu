bcbda714bba9e576040dd2b20f0af322f3bb4e55
marcsello <punkosdmarcell@rocketmail.com>
2020-04-03 03:03:03 +0200

    Objects with protected constructors no longer exposed

--- pyopenvidu/__init__.py
+++ pyopenvidu/__init__.py
@@ -5,8 +5,6 @@ __email__ = 'punkosdmarcell@rocketmail.com'
 __version__ = '0.1.0'
 
 from .openvidu import OpenVidu
-from .openvidusession import OpenViduSession
-from .openviduconnection import OpenViduConnection
 
 from .exceptions import OpenViduError, OpenViduSessionError, OpenViduSessionDoesNotExistsError, OpenViduConnectionError, \
     OpenViduConnectionDoesNotExistsError, OpenViduStreamError, OpenViduStreamDoesNotExistsError, OpenViduSessionExistsError
